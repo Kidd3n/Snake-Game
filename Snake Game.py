8480a02dd0c094698c8e4c0754bdcab3aaec8806
Kidden <89719224+Kidd3n@users.noreply.github.com>
2023-10-06 08:27:41 -0600

    Actualizar Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -6,20 +6,16 @@ pygame.init()
 
 WIDTH = 400
 HEIGHT = 400
-
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
-
 GRID_SIZE = 20
 GRID_WIDTH = WIDTH // GRID_SIZE
 GRID_HEIGHT = HEIGHT // GRID_SIZE
 SNAKE_SPEED = 10
-
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
-
 
 def draw_text(text, font, color, surface, x, y):
     text_obj = font.render(text, True, color)
@@ -27,26 +23,48 @@ def draw_text(text, font, color, surface, x, y):
     text_rect.center = (x, y)
     surface.blit(text_obj, text_rect)
 
+def show_game_over_screen(screen):
+    font = pygame.font.Font(None, 64)
+    text = font.render("¡Game Over!", True, (255, 0, 0))
+    text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 50))
+
+    font_small = pygame.font.Font(None, 36)
+    play_again_text = font_small.render("Presiona 'R' para jugar de nuevo", True, (0, 0, 255))
+    play_again_rect = play_again_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 20))
+
+    quit_text = font_small.render("Presiona 'Q' para salir", True, (0, 0, 255))
+    quit_rect = quit_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 60))
+
+    screen.blit(text, text_rect)
+    screen.blit(play_again_text, play_again_rect)
+    screen.blit(quit_text, quit_rect)
+    pygame.display.update()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    return True
+                elif event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit()
 
 def main():
-   
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Snake Game')
 
-   
     snake = [(5, 5)]
     snake_direction = RIGHT
 
-    
     food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
 
-    
     clock = pygame.time.Clock()
 
-  
     score = 0
 
-    
     key_mapping = {
         pygame.K_UP: UP,
         pygame.K_DOWN: DOWN,
@@ -58,17 +76,14 @@ def main():
         pygame.K_d: RIGHT,
     }
 
-  
     font = pygame.font.Font(None, 36)
 
-    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                
                 if event.key in key_mapping:
                     new_direction = key_mapping[event.key]
                     if (
@@ -81,29 +96,37 @@ def main():
         new_head = (head[0] + snake_direction[0], head[1] + snake_direction[1])
         snake.insert(0, new_head)
 
-        
         if snake[0] == food:
             score += 1
             food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
         else:
             snake.pop()
 
-       
         if (
             snake[0][0] < 0
             or snake[0][0] >= GRID_WIDTH
             or snake[0][1] < 0
             or snake[0][1] >= GRID_HEIGHT
         ):
-            pygame.quit()
-            sys.exit()
+            if show_game_over_screen(screen):
+                snake = [(5, 5)]
+                snake_direction = RIGHT
+                food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
+                score = 0
+            else:
+                pygame.quit()
+                sys.exit()
 
-       
         if snake[0] in snake[1:]:
-            pygame.quit()
-            sys.exit()
+            if show_game_over_screen(screen):
+                snake = [(5, 5)]
+                snake_direction = RIGHT
+                food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
+                score = 0
+            else:
+                pygame.quit()
+                sys.exit()
 
-      
         screen.fill(BLACK)
 
         for segment in snake:
